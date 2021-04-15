6b39097e31500135257e088f29a9ed60a39fab9b
Taylor Slye <slyet@oregonstate.edu>
2021-04-15 10:22:46 -0700

    line now returns diff based on param

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,10 @@
 class House
 
-  def line(_)
+  def line(number)
+  if number == 1
     return "This is the house that Jack built.\n"
+  else
+    "This is the malt that lay in  the house that Jack Built\n"
   end
 
 end
