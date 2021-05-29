e367f84bf4bfe3af59921451dbf10f6f213e5742
Taylor Slye <slyet@oregonstate.edu>
2021-05-29 15:03:35 -0700

    removed for not being green enough, restarting

--- lib/house.rb
+++ lib/house.rb
@@ -5,32 +5,7 @@ class House
   end
 
   def verse(number)
-    case number
-    when 1
-      "the house that Jack built"
-    when 2
-      "the malt that lay in " + verse(1)
-    when 3
-      "the rat that ate " + verse(2)
-    when 4
-      "the cat that killed " + verse(3)
-    when 5
-      "the dog that worried " + verse(4)
-    when 6
-      "the cow with the crumpled horn that tossed " + verse(5)
-    when 7
-      "the maiden all forlorn that milked " + verse(6)
-    when 8
-      "the man all tattered and torn that kissed " + verse(7)
-    when 9
-      "the priest all shaven and shorn that married " + verse(8)
-    when 10
-      "the rooster that crowed in the morn that woke " + verse(9)
-    when 11
-      "the farmer sowing his corn that kept " + verse(10)
-    when 12
-      "the horse and the hound and the horn that belonged to " + verse(11)
-    end
+
   end
   
   def recite
