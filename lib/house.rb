80f88b9141480586ff23b7a500a636c4e9967524
Taylor Slye <slyet@oregonstate.edu>
2021-05-29 19:06:35 -0700

    put first noun and verb into array

--- lib/house.rb
+++ lib/house.rb
@@ -9,7 +9,7 @@ class House
     when 1
       ''
     when 2
-      "malt that lay in the "
+      verbandnoun[0]
     when 3
       "rat that ate the malt that lay in the "
     when 4
@@ -31,6 +31,12 @@ class House
     when 12
       "horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the "
     end
+  end
+
+  def verbandnoun
+    [
+      "malt that lay in the "
+    ]
   end
   
   def recite
