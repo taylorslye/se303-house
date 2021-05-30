6adb8d19f6d0710d3259b6ae3d9fd159d8c476a5
Taylor Slye <slyet@oregonstate.edu>
2021-05-29 19:21:51 -0700

    moved verbs and noun sections into the corresponding array, reverse it and join to get the right order

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,23 @@
 class House
 
   def line(number)
-    return "This is the " + verse(number) + "house that Jack built.\n"
+    return "This is the " + nounandverbs.first(number - 1).reverse.join + "house that Jack built.\n"
+  end
+
+  def nounandverbs
+    [
+      "malt that lay in the ",
+      "rat that ate the ",
+      "cat that killed the ",
+      "dog that worried the ",
+      "cow with the crumpled horn that tossed the ",
+      "maiden all forlorn that milked the ",
+      "man all tattered and torn that kissed the ",
+      "priest all shaven and shorn that married the ",
+      "rooster that crowed in the morn that woke the ",
+      "farmer sowing his corn that kept the ",
+      "horse and the hound and the horn that belonged to the "
+    ]
   end
 
   def verse(number)
@@ -33,12 +49,7 @@ class House
     end
   end
 
-  def nounandverb
-    [
-      "malt that lay in the ",
-      "rat that ate the "
-    ]
-  end
+
   
   def recite
     1.upto(12).collect{ |i|
